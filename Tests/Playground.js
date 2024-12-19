2f296b75f82672627718e38db68e6b062e913399
Nuno Figueiredo <nuno.ferrao.figueiredo@gmail.com>
2024-12-19 10:08:28 +0000

    Finalized pAequorFactory exercise

--- Tests/Playground.js
+++ Tests/Playground.js
@@ -1,62 +1,76 @@
 const goat = {
-    dietType: 'herbivore',
-    makeSound() {
-      console.log('baaa');
-    },
-    diet() {
-      console.log(goat.dietType);
-    }
-  };
-  goat.diet(); 
-  // Output will be "ReferenceError: dietType is not defined"
-  
-  // debug test
-  const GPA_BENCHMARK = 3.5;
+  dietType: "herbivore",
+  makeSound() {
+    console.log("baaa");
+  },
+  diet() {
+    console.log(goat.dietType);
+  },
+};
+goat.diet();
+// Output will be "ReferenceError: dietType is not defined"
+
+// debug test
+const GPA_BENCHMARK = 3.5;
 let students = {
   1: {
-    name: 'Egill Vignission',
-    gpa: 3.4
+    name: "Egill Vignission",
+    gpa: 3.4,
   },
   2: {
-    name: 'Bianca Pargas',
-    gpa: 3.8
+    name: "Bianca Pargas",
+    gpa: 3.8,
   },
   3: {
-    name: 'Aisling O\'Sullivan',
-    gpa: 3.4
+    name: "Aisling O'Sullivan",
+    gpa: 3.4,
   },
   4: {
-    name: 'Sameer Fares',
-    gpa: 3.9
-  }
-}
- 
-let honorRoll = Object.values(students).filter(student => {
- return student.gpa >= GPA_BENCHMARK;
+    name: "Sameer Fares",
+    gpa: 3.9,
+  },
+};
+
+let honorRoll = Object.values(students).filter((student) => {
+  return student.gpa >= GPA_BENCHMARK;
 });
 
-
- 
 console.log(honorRoll);
 
 for (let x = 0; x < 10; x++) {
   console.log(x);
 }
 
-let instrument
-function setInstrument(instr) {    
-   instrument = instr;
-  console.log(instrument); 
-} 
+let instrument;
+function setInstrument(instr) {
+  instrument = instr;
+  console.log(instrument);
+}
 setInstrument("cello");
 
-let array1 = ['A', 'B', 'C', 'D', 'E', 'F']
-let randomIndex =(Math.floor(Math.random()* 5))
-while (array1[randomIndex] === 'B'){
-array1[randomIndex] = array1[randomIndex][Math.floor(Math.random() * 5)]
+let array1 = ["A", "B", "C", "D", "E", "F"];
+let randomIndex = Math.floor(Math.random() * 5);
+currentBase = array1[randomIndex];
+if (currentBase === "A") {
+  let newIndex = Math.floor(Math.random() * 5);
+  array1[randomIndex] = array1[newIndex];
+} else if (currentBase === "B") {
+  let newIndex = Math.floor(Math.random() * 5);
+  array1[randomIndex] = array1[newIndex];
+} else if (currentBase === "C") {
+  let newIndex = Math.floor(Math.random() * 5);
+  array1[randomIndex] = array1[newIndex];
+} else if (currentBase === "D") {
+  let newIndex = Math.floor(Math.random() * 5);
+  array1[randomIndex] = array1[newIndex];
+} else if (currentBase === "E") {
+  let newIndex = Math.floor(Math.random() * 5);
+  array1[randomIndex] = array1[newIndex];
+} else if (currentBase === "F") {
+  let newIndex = Math.floor(Math.random() * 5);
+  array1[randomIndex] = array1[newIndex];
+}
 
-} 
-  
-  console.log(array1[3])
-  console.log(array1)
-
+console.log(array1[3]);
+console.log(currentBase);
+console.log(array1);
--- Tests/pAequorFactory.js
+++ Tests/pAequorFactory.js
@@ -1,35 +1,117 @@
 // Returns a random DNA base
 const returnRandBase = () => {
-    const dnaBases = ["A", "T", "C", "G"];
-    return dnaBases[Math.floor(Math.random() * 4)];
-  };
-  
-  // Returns a random single strand of DNA containing 15 bases
-  const mockUpStrand = () => {
-    const newStrand = [];
-    for (let i = 0; i < 15; i++) {
-      newStrand.push(returnRandBase());
-    }
-    return newStrand;
-  };
-  
-//   console.log(mockUpStrand());
-  
-  const pAequorFactory = (specimenNum, dna) => {
-    return {
-      specimenNum: specimenNum,
-      dna: mockUpStrand(),
-      mutate() {
-        let mutatedGene;
-        for (i=0; i< dna.length; i++){
-        dna.length[Math.floor(Math.random() * 14)]
-        return [i];
-      }
-    }
+  const dnaBases = ["A", "T", "C", "G"];
+  return dnaBases[Math.floor(Math.random() * 4)];
+};
+
+// Returns a random single strand of DNA containing 15 bases
+const mockUpStrand = () => {
+  const newStrand = [];
+  for (let i = 0; i < 15; i++) {
+    newStrand.push(returnRandBase());
   }
+  return newStrand;
+};
+
+console.log(mockUpStrand());
+
+const pAequorFactory = (specimenNum) => {
+  return {
+    specimenNum: specimenNum,
+    dna: mockUpStrand(),
+    mutate() {
+      let randomIndex = Math.floor(Math.random() * 3);
+      let currentBase = this.dna[randomIndex];
+      console.log("mutating...");
+      if (currentBase === "A") {
+        let newIndex = Math.floor(Math.random() * 14);
+        this.dna[randomIndex] = this.dna[newIndex];
+      } else if (currentBase === "T") {
+        newIndex = Math.floor(Math.random() * 14);
+        this.dna[randomIndex] = this.dna[newIndex];
+      } else if (currentBase === "C") {
+        newIndex = Math.floor(Math.random() * 14);
+        this.dna[randomIndex] = this.dna[newIndex];
+      } else if (currentBase === "G") {
+        newIndex = Math.floor(Math.random() * 14);
+        this.dna[randomIndex] = this.dna[newIndex];
+      }
+      console.log(this.specimenNum + " has mutated");
+    },
+    compareDNA(samplePaequor) {
+      console.log("comparing DNA...");
+      let similarityCount = 0;
+      for (i = 0; i < this.dna.length; i++) {
+        if (this.dna[i] === samplePaequor.dna[i]) {
+          similarityCount++;
+        }
+      }
+      console.log(similarityCount);
+      let percentage = ((similarityCount / 15) * 100).toFixed(0);
+      console.log(percentage);
+      console.log(
+        this.specimenNum +
+          " and " +
+          samplePaequor.specimenNum +
+          " have " +
+          percentage +
+          "% DNA in common"
+      );
+    },
+    willLikelySurvive() {
+      let countCG = 0;
+      let percentageCG = 0;
+      for (i=0; i < this.dna.length; i++) {
+        if (this.dna[i] === 'C' || this.dna[i] === 'G') { countCG++}
+        
+        
+        }
+        console.log('Count of C and G: ' + countCG)
+        percentageCG = (countCG / 15 * 100).toFixed (0);
+       
+        console.log(percentageCG)
+        if (percentageCG >= 60) {
+          console.log('Specimen ' +this.specimenNum + ' is likely to survive')
+          return true;
+        } else {
+          console.log('Specimen ' + this.specimenNum + ' is unlikely to survive')
+          return false;
+    }
+      }
+      
+  };
+};
+// Create 2 pAequor samples using factory function
+const pAequor1 = pAequorFactory(1);
+const pAequor2 = pAequorFactory(2);
+
+// log DNA values for each specimen
+console.log("Specimen 1 DNA: " + pAequor1.dna);
+console.log("Specimen 2 DNA: " + pAequor2.dna);
+// Compare both samples DNA
+pAequor1.compareDNA(pAequor2);
+// mutate 1st sample
+pAequor1.mutate();
+
+// Determine if they are likely to survive
+console.log(pAequor1.willLikelySurvive())
+console.log(pAequor2.willLikelySurvive())
+
+
+let viableSpecimens = []
+for (let i = 3; i <=30; i++) {
+  let newPaequor = pAequorFactory(i);
+  newPaequor
+  console.log(newPaequor)
+
+  while (!newPaequor.willLikelySurvive()) {
+    newPaequor.dna = mockUpStrand(); 
+}
+viableSpecimens.push(newPaequor);
 }
   
-  const pAequor1 = pAequorFactory(1, mockUpStrand());
-  
-  console.log(pAequor1);
-  pAequor1.mutate()
+
+
+
+viableSpecimens.forEach(specimen => {console.log(specimen.willLikelySurvive())})
+
